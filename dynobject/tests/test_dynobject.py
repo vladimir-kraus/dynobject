bbf63f82e6f5fec59f1532d3e6e03a2f6f3ab96c
vladimir-kraus <vladimir.kraus@gmail.com>
2017-01-30 22:02:48 +0100

    improved unit tests

--- dynobject/tests/test_dynobject.py
+++ dynobject/tests/test_dynobject.py
@@ -1,6 +1,3 @@
-import os
-import sys
-
 import unittest
 
 from dynobject import DynObject
@@ -10,26 +7,39 @@ class DynObjectTests(unittest.TestCase):
 
     def test_empty(self):
         dobj = DynObject()
+        self.assertFalse(dobj)
+        self.assertEqual(len(dobj), 0)
         self.assertTrue(isinstance(self.__dict__, dict))
         self.assertEqual(dobj.__dict__, dict())
         self.assertFalse(dobj.__dict__)
         self.assertEqual(len(dobj), 0)
 
-    def test_non_empty(self):
+    def test_init_with_dict(self):
         source = dict(x=1, y=2)
         dobj = DynObject(source)
-        self.assertTrue(dobj.__dict__)
         self.assertEqual(dobj.__dict__, source)
         self.assertEqual(dobj.x, 1)
         self.assertEqual(dobj.y, 2)
         self.assertEqual(dobj["x"], 1)
         self.assertEqual(dobj["y"], 2)
 
+    def test_init_with_kwargs(self):
+        source = dict(x=1, y=2)
+        dobj = DynObject(x=1, y=2)
+        self.assertEqual(dobj.__dict__, source)
+        self.assertEqual(dobj.x, 1)
+        self.assertEqual(dobj.y, 2)
+        self.assertEqual(dobj["x"], 1)
+        self.assertEqual(dobj["y"], 2)
 
-if __name__ == "__main__":
-    path = os.path.dirname(os.path.realpath(__file__))
-    module_path = os.path.join(path, os.pardir, os.pardir)
-    sys.path.insert(0, module_path)
-    print("Testing in {}".format(path))
-    test_suite = unittest.defaultTestLoader.discover(path, pattern="*.py")
-    unittest.TextTestRunner(verbosity=2).run(test_suite)
+    def test_properties(self):
+        dobj = DynObject(x=1, y=2)
+        self.assertTrue(dobj)
+        self.assertEqual(len(dobj), 2)
+        self.assertEqual(dobj.x, 1)
+        self.assertEqual(dobj.y, 2)
+        self.assertEqual(dobj["x"], 1)
+        self.assertEqual(dobj["y"], 2)
+        self.assertListEqual(dir(dobj), ["x", "y"])
+        self.assertEqual(str(dobj), str(dobj.__dict__))
+        self.assertEqual(repr(dobj), repr(dobj.__dict__))
